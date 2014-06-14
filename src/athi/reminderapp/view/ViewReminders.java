d24265329920e8bfeb92abc1b0ce8a8df3558b61
Athiruban SM <ruban.athi@gmail.com>
2014-06-14 20:42:41 +0530

    reformatted and decoupled the logic in viewreminder class

--- src/athi/reminderapp/view/ViewReminders.java
+++ src/athi/reminderapp/view/ViewReminders.java
@@ -15,44 +15,69 @@ import athi.reminderapp.model.Reminder;
 import athi.reminderapp.model.ReminderList;
 import athi.reminderapp.scheduler.EventScheduler;
 
-public class ViewReminders extends JFrame {
-	Container c = getContentPane();
+/* In the new version of the application, support for table will be added.
+ * The JTable class provides a good look for the old expired and future reminders
+ * This class will be modified to accommodate the new changes.
+ */
 
-	public ViewReminders() {
-		
-		EventScheduler eventScheduler = EventScheduler.getInstance();
-		List<Reminder> remlist = eventScheduler.pullEvents();
-		
-		int remcount=remlist.size();
-		
-		if (remcount == 0) {
-			setLayout(null);
-			this.setSize(200, 200);
-			this.setTitle("View Reminders Panel");
-			this.setVisible(true);
-			JLabel l1 = new JLabel("No active reminders");
-			l1.setBounds(50, 100, 100, 20);
-			c.add(l1);
-			return;
-		}
-		
-		setLayout(null);
-		this.setSize(500, (remcount * 50) + 100);
-		this.setTitle("View Reminders Panel");
+public class ViewReminders extends JFrame {
+	Container viewContainer;
+	private List<Reminder> remlist;
+	private EventScheduler eventScheduler;
+	private int remcount; 
+	
+	private void init(){
+		eventScheduler = EventScheduler.getInstance();
+		remlist = eventScheduler.pullEvents();
+		remcount = remlist.size();
+	}
+	
+	private void populateEmptyView(){
+		this.setSize(200, 200);
+		this.setTitle("View Reminders Panel:No active reminders!");
 		this.setVisible(true);
+		/*
+		 * Empty View
+		 */
+		JLabel l1 = new JLabel("No active reminders");
+		l1.setBounds(50, 100, 100, 20);
+		viewContainer.add(l1);
+	}
+	
+	private void populateNormalView(){
+		/* 
+		 * Based on the reminder size the height of the window will be determined
+		 */
+		this.setSize(500, (remcount * 50) + 100);
+		this.setTitle("(:View Reminders Panel:)");
+		this.setVisible(true);
+		/*
+		 * Normal view
+		 */
 		JButton[] titlelist = new JButton[remcount];
 		System.out.println(remcount);
 		
 		int i = 0;
 		for ( ;i<remcount; i++) {
-			//Reminder r = remlist.get(i);
 			String buttontext = remlist.get(i).getReminderTitle() + " @ "
 					+ remlist.get(i).getActivationTime();
 			titlelist[i] = new JButton(buttontext);
 			titlelist[i].setBounds(50, 50 + (i * 50), 400, 30);
 			titlelist[i].addActionListener(new ViewRemindersHandler());
-			c.add(titlelist[i]);
+			viewContainer.add(titlelist[i]);
 			i++;
 		}
+	}
+	
+	public ViewReminders() {
+		viewContainer = this.getContentPane();
+		setLayout(null);
+		
+		init();
+		
+		if (remcount == 0)
+			populateEmptyView();
+		else
+			populateNormalView();
 	}
 }
