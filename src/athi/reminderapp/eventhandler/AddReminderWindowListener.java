5d8f9aa8da37d684f9295b99f43c352c6f968e0a
Athiruban SM <ruban.athi@gmail.com>
2014-06-14 23:01:28 +0530

    changed to the exithandler to save reminder objects in an adhoc basis

--- src/athi/reminderapp/eventhandler/AddReminderWindowListener.java
+++ src/athi/reminderapp/eventhandler/AddReminderWindowListener.java
@@ -2,18 +2,14 @@ package athi.reminderapp.eventhandler;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
 
-import athi.reminderapp.config.AppConfig;
-import athi.reminderapp.controller.MyApp;
-import athi.reminderapp.model.ReminderList;
+import athi.reminderapp.controller.IOManager;
 
 public class AddReminderWindowListener extends WindowAdapter{
 	@Override
 	public void windowClosing(WindowEvent arg0) {
-		MyApp.getInstance().saveReminders();
+		synchronized(this){
+			new IOManager().saveReminders();
+		}
 	}
 }
