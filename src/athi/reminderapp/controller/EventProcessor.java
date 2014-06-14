a750f84c63fe48c277ff751b819c3f42156f8f9a
Athiruban SM <ruban.athi@gmail.com>
2014-06-14 21:02:34 +0530

    Added a separate class to carry out the application functionality

---
+++ src/athi/reminderapp/controller/EventProcessor.java
@@ -0,0 +1,7 @@
+package athi.reminderapp.controller;
+
+public class EventProcessor {
+	/* The purpose of this class is to carry out the functionality of saving and retrieving 
+	 * reminders from file.
+	 */
+}
