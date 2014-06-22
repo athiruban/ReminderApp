59a7869274101da19a3a80a1326bf01e1fd79590
Athiruban SM <ruban.athi@gmail.com>
2014-06-22 19:49:36 +0530

    some thoughts!

--- src/athi/reminderapp/model/Reminder.java
+++ src/athi/reminderapp/model/Reminder.java
@@ -3,6 +3,25 @@ package athi.reminderapp.model;
 public class Reminder implements IReminder{
 	/**
 	 * This is the core of the application data
+	 * 
+	 * Every reminder object should hold an unique key to identify it easily without needing
+	 * to search the entire list....
+	 * Who will use the unique key?
+	 * - Removing reminder methods
+	 * Frequency?
+	 * - Less
+	 * 
+	 * Can reminder be grouped on something else?
+	 * Yes using time-frame we can group events easily.
+	 * Who will use the time based retrieval?
+	 * - Display methods
+	 * Frequency?
+	 * - Moderate
+	 * 
+	 * How about when we move reminder data to SQLite database?
+	 * It is obvious that reminder will be searched based on the time, location and other factors
+	 * so it should have a mechanism to persist those information by itself.
+	 *  
 	 */
 	private static final long serialVersionUID = 538551431272156716L;
 	private String reminderDesc;
